d3854fd4a595246374cde2260fda2fd24f356ec0
Elijah Bowers <71912982+emcsteez@users.noreply.github.com>
2022-07-11 14:59:49 -0400

    Delete FirstJava.java

--- FirstJava.java
+++
@@ -1,7 +0,0 @@
-public class FirstJava {
-    public static void main(String[] args){
-        System.out.println("My name is Elijah Bowers");
-        System.out.println("I am 29 years old");
-        System.out.println("My hometown is Keene, NH");
-    }
-}
