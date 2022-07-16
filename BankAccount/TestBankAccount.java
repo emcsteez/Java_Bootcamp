bc351c3b6894e57ccd099bf6836378ec2e608fc6
Elijah Bowers <71912982+emcsteez@users.noreply.github.com>
2022-07-16 14:09:58 -0400

    Add files via upload

---
+++ BankAccount/TestBankAccount.java
@@ -0,0 +1,25 @@
+package BankAccount;
+class TestBankAccount {
+    public static void main(String[] args) {
+		BankAccount ba = new BankAccount();
+        System.out.println("New Acc No. - " + ba.getAccountNumber());
+		ba.depositMoney(100.49, "checking");
+		ba.depositMoney(3.00, "savings");
+		ba.displayAccountBalance();
+
+		System.out.println(BankAccount.totalHoldings);
+
+		ba.withdrawMoney(50, "checking");
+		ba.withdrawMoney(50, "savings");
+		ba.displayAccountBalance();
+
+		System.out.println(BankAccount.totalHoldings);
+
+        BankAccount ba2 = new BankAccount();
+        System.out.println("New Acc No. - " + ba2.getAccountNumber());
+		ba2.depositMoney(3, "checking");
+		ba2.depositMoney(99, "savings");
+		ba2.displayAccountBalance();
+        ba2.withdrawMoney(50, "checking");
+	}
+}
