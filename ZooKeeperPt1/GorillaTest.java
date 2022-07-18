d490287a6af0134e6495cbb2a610f9cd6fa1e0a5
Elijah Bowers <71912982+emcsteez@users.noreply.github.com>
2022-07-17 23:01:32 -0400

    Add files via upload

---
+++ ZooKeeperPt1/GorillaTest.java
@@ -0,0 +1,14 @@
+package ZooKeeperPt1;
+
+public class GorillaTest {
+    
+    public static void main(String[] args) {
+        Gorilla gorilla = new Gorilla(100);
+        gorilla.throwSomething();
+        gorilla.throwSomething();
+        gorilla.throwSomething();
+        gorilla.eatBanana();
+        gorilla.eatBanana();
+        gorilla.climb();
+    }
+}
